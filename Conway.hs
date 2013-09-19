2a4cdcc193724c8f07c2fb4afb6661a51b992e03
Justin Leitgeb <justin@stackbuilders.com>
2013-09-19 17:51:31 -0500

    Refactoring

--- Conway.hs
+++ Conway.hs
@@ -1,42 +1,31 @@
 import qualified Data.List as L
 
-vecinosEstado :: Bool -> [Bool]->Int
-vecinosEstado estado vecinos = length $
-                               filter (\x -> x == estado)
-                               vecinos
+vecinosVivos :: [Bool]->Int
+vecinosVivos vecinos = length $
+                          filter (\x -> x == True)
+                          vecinos
 
-vecinosVivos :: [Bool] -> Int
-vecinosVivos vecinos = vecinosEstado True vecinos
+reglaUno :: Bool -> Int -> Bool
+reglaUno elemento numeroVivos = numeroVivos < 2
 
-vecinosMuertos :: [Bool] -> Int
-vecinosMuertos vecinos = vecinosEstado False vecinos
-
-reglaUno :: Bool -> [Bool] -> Bool
-reglaUno elemento vecinos =
-    if vecinosVivos vecinos < 2 then
-       False
-    else
-       True
-
-reglaDos :: Bool -> [Bool] -> Bool
-reglaDos elemento vecinos =
+reglaDos :: Bool -> Int -> Bool
+reglaDos elemento numeroVivos =
     if L.elem numeroVivos [2,3] && elemento then
        True
     else
        False
-    where numeroVivos = vecinosVivos vecinos
 
-reglaTres :: Bool -> [Bool] -> Bool
-reglaTres elemento vecinos
+reglaTres :: Bool -> Int -> Bool
+reglaTres elemento numeroVivos
           | elemento == True &&
-            vecinosVivos vecinos > 3 = False
-          | otherwise                = elemento
+            numeroVivos > 3 = False
+          | otherwise       = elemento
 
-reglaCuatro :: Bool -> [Bool] -> Bool
-reglaCuatro elemento vecinos
+reglaCuatro :: Bool -> Int -> Bool
+reglaCuatro elemento numeroVivos
     | elemento == False &&
-      vecinosVivos vecinos == 3 = True
-    | otherwise                 = elemento
+      numeroVivos == 3 = True
+    | otherwise        = elemento
 
 conway :: Bool -> [Bool] -> Bool
 conway elemento vecinos = undefined
